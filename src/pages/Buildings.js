b013e73bb91dbabef288b8b3064c223c8569b33a
ledieuelephant <contact.raphaelenlive@gmail.com>
2023-01-30 02:43:51 +0100

    Remove useless

--- src/pages/Buildings.js
+++ src/pages/Buildings.js
@@ -2,8 +2,6 @@ import { useEffect, useState } from "react";
 
 import Caroussel from "../components/Caroussel";
 import Error from "../components/Error";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
 import PageScrollerOptions from "../components/PageScrollerOptions";
 import PageScrollerText from "../components/PageScrollerText";
 import Tag from "../components/TAG";
